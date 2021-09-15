1dbb7567cbb39f1abdcdc4008aefbfddc6bee750
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2021-09-15 19:19:33 -0300

    initial commit

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,8 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    @State var knocking: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack {
+                Text("Welcome to my first app!")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.green)
+                Button(action: {
+                    consoleLogging(value: "Button pressed!")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Hello There!"), message: Text("This is my first pop-up!"), dismissButton: .default(Text("Awesome!"))
+                    )
+                }
+            }
+            Button(action: {
+                consoleLogging(value: "knocking")
+                self.knocking = true
+            }) {
+                Text("Knock, Knock!")
+            }
+            .alert(isPresented: $knocking) { () -> Alert in
+                return Alert(title: Text("Who's There?"), message: Text("A Swift developer!"), dismissButton: .default(Text("Good one!")))
+            }
+        }
     }
 }
 
@@ -18,4 +48,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func consoleLogging(value: String) -> Void {
+    print(value)
 }
